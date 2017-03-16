6d203cbc1e7734bc35a8a641903e104485c3a73a
Vlad Hociotă <vlad.hociota@geminisols.com>
2017-03-16 15:33:56 +0200

    add test

--- Tests/DataStoreTests/DataStoreTests.swift
+++ Tests/DataStoreTests/DataStoreTests.swift
@@ -2,21 +2,24 @@ import XCTest
 @testable import DataStore
 
 class DataStoreTests: XCTestCase {
-    func testJSONContent() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let jsonString = "{\"name\":\"value\"}"
-        let data = jsonString.data(using: .utf8)
+    func testJSONContentEncode() {
         let jsonContent = DataStoreContentJSONDictionary<String,String>()
-        let result = jsonContent.fromData(data!)
-        print(result ?? "success")
-        //XCTAssertEqual(result, nil)
+        jsonContent.set("value", for: "name")
+        let result = jsonContent.toData()
+        XCTAssertEqual(result, nil)
     }
 
+    func testJSONContentDecode() {
+        let jsonContent = DataStoreContentJSONDictionary<String,String>()
+        let data = "{\"name\":\"value\"}".data(using: .utf8)!
+        let result = jsonContent.fromData(data)
+        XCTAssertEqual(result, nil)
+    }
 
     static var allTests : [(String, (DataStoreTests) -> () throws -> Void)] {
         return [
-            ("testJSONContent", testJSONContent),
+            ("testJSONContentEncode", testJSONContentEncode),
+            ("testJSONContentDecode", testJSONContentDecode)
         ]
     }
 }
